52cc05b2a78b7d17dedd6f824bbacf815766192d
Francisco Chaves <francisco.jschaves@gmail.com>
2019-06-05 10:16:28 -0300

    Creating site with navigation

--- index.js
+++ index.js
@@ -2,11 +2,14 @@ const express = require('express');
 const app = express();
 
 app.set('view engine', 'ejs');
+app.use(express.static('public'));
 
 app.get('/', (request, response) => {
-	response.render('home', {
-		date: new Date()
-	});
+	response.render('home');
+});
+
+app.get('/vaga', (request, response) => {
+	response.render('vaga');
 });
 
 app.listen(3000, (err) => {
