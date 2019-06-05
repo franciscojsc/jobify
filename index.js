d531ad4c33a803e21ea54bcb224bc13d3931ca0e
Francisco Chaves <francisco.jschaves@gmail.com>
2019-06-05 11:22:47 -0300

    Creating database

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 
+const sqlite = require('sqlite');
+const dbConnection = sqlite.open('banco.sqlite', (Promise) => {});
+
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
@@ -11,6 +14,15 @@ app.get('/', (request, response) => {
 app.get('/vaga', (request, response) => {
 	response.render('vaga');
 });
+
+const init = async () => {
+	const db = await dbConnection;
+	await db.run('create table if not exists categorias (id INTEGER PRIMARY KEY, categoria TEXT);');
+	//const categoria = 'Engineering team';
+	//await db.run(`insert  into categorias(categoria) values('${categoria}');`);
+};
+
+init();
 
 app.listen(3000, (err) => {
 	if (err) {
